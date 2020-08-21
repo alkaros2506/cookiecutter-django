69285e2aa32bdbee3988cf54d23ab2228e4aa725
alkis.sellis <alkis.sellis@invitae.com>
2020-08-21 14:07:47 -0700

    Adding some back

--- {{cookiecutter.project_slug}}/config/api_router.py
+++ {{cookiecutter.project_slug}}/config/api_router.py
@@ -1,7 +1,11 @@
+from typing import Union
+
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from {{ cookiecutter.project_slug }}.users.api.views import UserViewSet
+
+router: Union[DefaultRouter, SimpleRouter]
 
 if settings.DEBUG:
     router = DefaultRouter()
